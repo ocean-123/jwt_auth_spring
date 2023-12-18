d66f687684c98fff4d7610629e3edba3e107dea0
ocean-123 <90921256+ocean-123@users.noreply.github.com>
2023-12-18 14:17:23 +0545

    package are created

--- src/main/java/com/example/jwt_auth_simple/entity/Client.java
+++ src/main/java/com/example/jwt_auth_simple/entity/Client.java
@@ -1,4 +1,29 @@
 package com.example.jwt_auth_simple.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
 public class Client {
+
+
+    @Id
+    private Long id;
+
+    private  String username;
+    private  String  email;
+     private String password;
+
+
+
 }
