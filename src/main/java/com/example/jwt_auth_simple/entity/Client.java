9f5a43d12476862ff8e468307f236ce553d05bfa
ocean-123 <90921256+ocean-123@users.noreply.github.com>
2023-12-18 12:45:14 +0545

    package are created

---
+++ src/main/java/com/example/jwt_auth_simple/entity/Client.java
@@ -0,0 +1,4 @@
+package com.example.jwt_auth_simple.entity;
+
+public class Client {
+}
---
+++ src/test/java/com/example/jwt_auth_simple/JwtAuthSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt_auth_simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
